9f3a952bc36066894fa10a56029a72dcc7c92ba3
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-25 15:56:23 +0700

    1 file modified

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,22 +4,19 @@
     <ul>
       <li>Home</li>
       <li>About</li>
-      <li v-if="isAuthenticated">
-        Total todos: {{ todos.length }}
-        <button @click="TOGGLE_AUTH">Logout</button>
-      </li>
-      <button v-else @click="TOGGLE_AUTH">Login</button>
+      <li>Total todos: {{ todos.length }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import { mapMutations, mapGetters } from 'vuex'
-
 export default {
   name: 'Navbar',
-  computed: mapGetters(['todos', 'isAuthenticated']),
-  methods: mapMutations(['TOGGLE_AUTH']),
+  computed: {
+    todos() {
+      return this.$store.state.todos
+    },
+  },
 }
 </script>
 
